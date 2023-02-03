71517e33f025940ccb6df50f74ab3b63a5f76d37
ElifKartal <elifkartal2500@gmail.com>
2023-02-03 20:43:56 +0300

    message

--- ClassDeneme.java
+++
@@ -1,7 +0,0 @@
-public class ClassDeneme {
-    public static void main(String[] args) {
-        Class1.print("merhaba");
-        int x Class1.sayiToplayici(2,5);
-        System.out.println(x);
-    }
-}

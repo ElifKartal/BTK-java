7c85c2acae4604e0cd864748932eb0493296c318
ElifKartal <elifkartal2500@gmail.com>
2023-02-04 16:22:57 +0300

    .

---
+++ Methods/ErkekOgrenci.java
@@ -0,0 +1,16 @@
+package Methods;
+
+public class ErkekOgrenci extends Ogrenci {
+    private String isim;
+    private int numara;
+
+    public ErkekOgrenci(String isim, int numara) {
+        super(isim, numara);
+        this.isim = isim;
+        this.numara = numara;
+    }
+
+    public String ogrencininIsmi() {
+        return "erkek ogrenci: " + isim;
+    }
+}
---
+++ Methods/Ogrenci.java
@@ -0,0 +1,24 @@
+package Methods;
+
+public class Ogrenci extends Insan {
+    private String isim;
+    private int numara;
+
+    public Ogrenci(String isim, int numara) {
+        super(isim);
+        this.isim = isim;
+        this.numara = numara;
+    }
+
+    public String ogrencininIsmi() {
+        return isim;
+    }
+
+    public int ogrencininNumarasi() {
+        return numara;
+    }
+
+    public void publicMetodaGidis() {
+        super.publicMetot();
+    }
+}

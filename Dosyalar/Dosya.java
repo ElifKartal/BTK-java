77800ee080731974fab5e561bed270b0218ef5ff
ElifKartal <elifkartal2500@gmail.com>
2023-02-18 01:16:57 +0300

    .

--- Dosyalar/Dosya.java
+++ Dosyalar/Dosya.java
@@ -1,29 +1,57 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Dosya {
     public static void main(String[] args) {
-        File dosyaFile = new File("C:/Users/Elif/Desktop/BTKjava/Dosyalar/text.txt");
-        System.out.println(dosyaFile.exists());
-        System.out.println(dosyaFile.getAbsolutePath());
-        System.out.println(dosyaFile.getName());
-        System.out.println(dosyaFile.getParentFile());
-        System.out.println(dosyaFile.setLastModified(16766503550000l));
+        File dosya = new File("C:/Users/otota/Desktop/BTK/Gun7/text.txt");
+        System.out.println(dosya.exists());
+        System.out.println(dosya.getAbsolutePath());
+        System.out.println(dosya.getName());
+        System.out.println(dosya.getParentFile());
+        System.out.println(dosya.setLastModified(16766503550000000l));
         Scanner inputStream = null;
-        Scanner javaInputStream = null;
         try {
-             inputStream =new Scanner(dosyaFile);
-             javaInputStream =new Scanner(new File("Dosyalar.java"));
+            inputStream = new Scanner(dosya);
         } catch (FileNotFoundException e) {
             System.out.println("Dosya bulunamadi");
         }
-        System.out.println(inputStream.nextLine());
-        System.out.println(javaInputStream.next());
+
+        String paragraf = "";
+        while (inputStream.hasNextLine())
+            paragraf += inputStream.nextLine() + "\n";
+        System.out.println(paragraf);
+        System.out.println(paragraf.contains("hey"));
+        inputStream.close();
+
+        PrintWriter javaOutputStream = null;
+        PrintWriter outputStream = null;
+        try {
+            outputStream = new PrintWriter(dosya);
+        } catch (FileNotFoundException e) {
+            System.out.println("Dosya bulunamadi");
+        }
         
+        outputStream.print(paragraf);
+        
+        outputStream.close();
 
-
-
+        // for (int i = 0; i < 5; i++) {
+        //     File javaDosyasi = new File("Dosyalar.txt");
+        //     javaDosyasi.delete();
+        // }
+        
+        // while (true) {
+        //     File folder = new File("folder");
+        //     folder.delete();
+        //     folder.mkdir();
+        // }
+        
+        // ArrayList objects = new ArrayList<>();
+        // while(true) {
+        //     objects.add(new Excep());
+        // }
     }
-    
-    
 }
